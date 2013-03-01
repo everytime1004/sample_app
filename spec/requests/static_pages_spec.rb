3303c0800aabec89fa8f07a97f2d4bcc4deb427f
everytime1004 <ha621209@naver.com>
2013-03-01 17:50:53 +0900

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,16 +42,32 @@ describe "Static pages" do
     it_should_behave_like "all static pages"
   end
 
-  it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    page.should have_selector 'title', text: full_title("About")
-    click_link "Help"
-    page.should have_selector 'title', text: full_title('Help')
-    click_link "Contact"
-    page.should have_selector 'title', text: full_title('Contact')
-    click_link "Home"
-    click_link "sign up now!"
-    page. should have_selector 'h1', text: "Sign up"
+  describe "for signed-in users" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+      FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+      sign_in user
+      visit root_path
+    end
+
+    it "should render the user's feed" do
+      user.feed.each do |item|
+        page.should have_selector("li##{item.id}", text: item.content)
+      end
+    end
   end
+
+  # it "should have the right links on the layout" do
+  #   visit root_path
+  #   click_link "About"
+  #   page.should have_selector 'title', text: full_title("About")
+  #   click_link "Help"
+  #   page.should have_selector 'title', text: full_title('Help')
+  #   click_link "Contact"
+  #   page.should have_selector 'title', text: full_title('Contact')
+  #   click_link "Home"
+  #   click_link "sign up now!"
+  #   page. should have_selector 'h1', text: "Sign up"
+  # end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -57,10 +57,19 @@ describe "User pages" do
 
 	describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup" do
@@ -72,6 +81,13 @@ describe "User pages" do
   		it "should not create a user" do
   			expect {click_button submit}.not_to change(User, :count)
   		end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
   	end
 
   	describe "with valid information" do
@@ -88,7 +104,10 @@ describe "User pages" do
 
       describe "after saving the user" do 
         before{ click_button submit } 
-        it { should have_link('Sign out') }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
   	end
   end
